a2f1371fa22fd76145c4c4ac45b2c8cfddb648eb
mszymczak <mszymcza@gmail.com>
2023-02-08 10:56:44 -0700

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -944,7 +944,7 @@ xui.Class('App', 'xui.Module',{
             //this.xui_ui_labelBOMStarlingSupportSKU.setCaption(xui_ui_comboinput_TermsPeriodofPerformance.getUIValue());
             this.xui_ui_labelBOMStarlingSupportQty.setCaption("1");
             //xui_ui_labelBOMStarlingSupportDescription
-            this.xui_ui_labelBOMStarlingServicesSKU.setCaption(xui_ui_comboinput_TermsSupportType.getUIValue());
+            //this.xui_ui_labelBOMStarlingServicesSKU.setCaption(xui_ui_comboinput_TermsSupportType.getUIValue());
             this.xui_ui_labelBOMStarlingServicesQty.setCaption("1");
             //xui_ui_labelBOMStarlingServicesDescription
             
